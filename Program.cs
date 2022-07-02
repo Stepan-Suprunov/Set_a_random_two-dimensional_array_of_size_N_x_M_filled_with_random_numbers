e791c7073c8cee6036f9ae153e4cd84616b12a99
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-07-02 22:45:29 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать случайный двумерный массив размером N x M, заполненный случайными числами
+
+
+int m = 3, n = 4;
+int[,] mass = new int[m,n];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        //Console.Write(i + " " + j + ", ");
+        mass[i,j] = new Random().Next(-99,100);
+        Console.Write(mass[i,j] + " ");
+    }
+    Console.WriteLine();
+}
